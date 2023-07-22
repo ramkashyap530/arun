4a83c643499c4d73d774a50875bdd8c693bfdce7
ramkashyap530 <ramukashyap85@gmail.com>
2023-07-23 01:48:59 +0530

    aaa

--- index.js
+++ index.js
@@ -2,4 +2,12 @@ const http=require('http');
 const app=require('./app');
 const server=http.createServer(app);
 
-server.listen(3000,console.log('app is running'));
+// server.listen(3000,console.log('app is running'));
+
+// Use PORT provided in environment or default to 3000
+const port = process.env.PORT || 3000;
+
+// Listen on `port` and 0.0.0.0
+app.listen(port, "0.0.0.0", function () {
+  // ...
+});
